07bf0e66a4d17edb5813175cb3b03ed74e369b1c
Tony <93945730+Gosui@users.noreply.github.com>
2022-05-01 21:50:03 -0700

    Update SkillDemo.java

--- SkillDemo.java
+++ SkillDemo.java
@@ -2,7 +2,7 @@ public class SkillDemo {
 
     public static int sequentialAdd(int num){
         int total = 0;
-        for (int i = 0; i < num; i++){
+        for (int i = 0; i <= num; i++){
             total += i;
         }
         return total;
